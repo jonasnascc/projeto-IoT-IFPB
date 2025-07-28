1d3b2877f654f989480ea4bd394c375e4c432c77
jonasnascc <jonashnascimento@gmail.com>
2025-07-28 13:08:49 -0300

    rotas para listar elementos da tabela baseado em paginacao

--- servidor/routes.js
+++ servidor/routes.js
@@ -2,6 +2,37 @@ const express = require("express")
 const router = express.Router()
 const db = require('./models');
 
+const listFn = async (req, res, table) => {
+    try {
+        const limit = parseInt(req.query.limit) || 10;
+        const offset = parseInt(req.query.offset) || 0;
+
+        const list = await db[table].findAll({
+            limit,
+            offset,
+            order: [['timestamp', 'DESC']],
+        });
+
+        const total = await db[table].count();
+
+        const hasMore = offset + list.length < total;
+
+        res.status(200).json({
+            limit,
+            offset,
+            data: list,
+            hasMore,
+            total
+        });
+    } catch (err) {
+        console.error("Erro ao buscar mensagens:", err);
+        res.status(500).json({ error: "Erro interno do servidor" });
+    }
+}
+
+//-----------------//
+router.get("/portao", async (req, res) => await listFn(req, res, "PortaoMensagens"))
+
 router.get("/portao/last", async (req, res) => {
     const last = await db.PortaoMensagens.findOne({
         order: [['timestamp', 'DESC']],
@@ -9,6 +40,8 @@ router.get("/portao/last", async (req, res) => {
 
     res.status(200).json(last);
 })
+//-----------------//
+router.get("/caixa", async (req, res) => await listFn(req, res, "CaixaMensagens"))
 
 router.get("/caixa/last", async (req, res) => {
     const last = await db.CaixaMensagens.findOne({
@@ -17,6 +50,9 @@ router.get("/caixa/last", async (req, res) => {
 
     res.status(200).json(last);
 })
+
+//-----------------//
+router.get("/energia", async (req, res) => await listFn(req, res, "EnergiaMensagens"))
 
 router.get("/energia/last", async (req, res) => {
     const last = await db.EnergiaMensagens.findOne({
